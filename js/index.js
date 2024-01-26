64b2a7b4855c71e332b54888a7520d6a68f0357f
Melissa Martindale <mmartindale3@nait.ca>
2024-01-25 20:05:55 -0700

    Assigned IDs. Wrote JS for open and close buttons

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
+// call-to-action button
+const openModalButton = document.querySelector("#open-modal");
+
+// dialog element
+const dialogElement = document.querySelector("#dialog-modal");
+
+// close dialog button
+const closeDialogButton = document.querySelector("#close-modal");
+
+// call-to-action button
+openModalButton.addEventListener("click", onClickOpenButton);
  
+function onClickOpenButton(e) {
+   dialogElement.showModal();
+}
+
+// close dialog button
+closeDialogButton.addEventListener("click", onClickCloseButton);
+
+function onClickCloseButton(e) {
+   dialogElement.close();
+}
